7ac8f71bedff44de148142fbe45e3f3e8398e8e4
fraggatron <jonahyimsung@gmail.com>
2023-12-02 22:00:53 +0800

    Update TestMatrix.cs

--- Assets/02_MATRICES_worksheet/Sonic/TestMatrix.cs
+++ Assets/02_MATRICES_worksheet/Sonic/TestMatrix.cs
@@ -1,7 +1,8 @@
+// Uncomment this whole file.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class TestMatrix : MonoBehaviour
 {
@@ -13,41 +14,6 @@ public class TestMatrix : MonoBehaviour
         mat.Print();
     }
 
-    private void Question2()
-    {
-        // Hardcode 3x3 matrices
-        HMatrix2D mat1 = new HMatrix2D
-        (
-            1, 2, 3,
-            4, 5, 6,
-            7, 8, 9
-        );
 
-        HMatrix2D mat2 = new HMatrix2D
-        (
-            9, 8, 7,
-            6, 5, 4,
-            3, 2, 1
-        );
-
-        HMatrix2D resultMat;
-
-        // Hardcode 3x1 vector
-        HMatrix2D.HVector2D vec1 = new HMatrix2D.HVector2D(2, 3);
-
-        // Perform 3x3 * 3x3 matrix multiplication
-        resultMat = mat1 * mat2;
-
-        // Print the result
-        Console.WriteLine("Matrix Multiplication Result:");
-        Console.WriteLine(resultMat);
-
-        // Perform 3x3 * 3x1 matrix multiplication
-        HMatrix2D.HVector2D resultVec = mat1 * vec1;
-
-        // Print the result
-        Console.WriteLine("Vector Multiplication Result:");
-        Console.WriteLine($"X: {resultVec.X}, Y: {resultVec.Y}");
-    }
+    
 }
-
