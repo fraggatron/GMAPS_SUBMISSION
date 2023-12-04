e26ab97e9712d03b5d8680f31b905d5e62bcaf81
fraggatron <jonahyimsung@gmail.com>
2023-12-04 12:37:49 +0800

    1

--- Assets/03_KINEMATICS_Worksheet/Part 1/Force/Motion.cs
+++ Assets/03_KINEMATICS_Worksheet/Part 1/Force/Motion.cs
@@ -1,19 +1,19 @@
- using System.Collections;
- using System.Collections.Generic;
- using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
- public class Motion : MonoBehaviour
- {
-     public Vector3 Velocity;
+public class Motion : MonoBehaviour
+{
+    public Vector3 Velocity;
 
-     void FixedUpdate()
-     {
-         float dt = Time.deltaTime;
+    void FixedUpdate()
+    {
+        float dt = Time.deltaTime;
 
-         float dx = Velocity.x * dt;
-         float dy = Velocity.y * dt;
-         float dz = Velocity.z * dt;
+        float dx = Velocity.x * dt;
+        float dy = Velocity.y * dt;
+        float dz = Velocity.z * dt;
 
-         transform.Translate(new Vector3(dx, dy, dz));
-     }
- }
+        transform.Translate(new Vector3(dx, dy, dz));
+    }
+}
